742fc4cba9df15964336e53b7647c1660605a3dd
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2024-11-12 19:13:56 +0600

    Easy programming

---
+++ Module 17/17-7 Change Password using and without using old password/eight_project/first_app/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render,redirect
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import update_session_auth_hash
+ 
+
+# Create your views here.
+def home(request):
+    return render(request, './home.html')
+
+
+def signup(request):
+    if not request.user.is_authenticated:        
+        if request.method=='POST':
+            form=RegisterForm(request.POST)
+            if form.is_valid():
+                messages.success(request,'Account createrd Successfully')
+                messages.warning(request,'Warning')
+                messages.info(request,'Info')
+                form.save(commit=True)
+                print(form.cleaned_data)
+        else:
+            form=RegisterForm()       
+        return render(request,'signup.html',{'form':form})
+    else:
+        return redirect('profile')
+
+
+def user_login(request):
+    if not request.user.is_authenticated:
+        
+        if request.method == 'POST':
+            form=AuthenticationForm(request=request,data=request.POST)
+            if form.is_valid():
+                name=form.cleaned_data['username']
+                userpass=form.cleaned_data['password']
+                user=authenticate(username=name,password=userpass)#check kortechi user database e ache kina
+                if user is not None:
+                    login(request,user)
+                    return redirect('profile')
+        else:
+            form=AuthenticationForm()
+        return render(request,'./login.html',{'form':form})
+    
+    else:
+        return redirect('profile')
+    
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request,'./profile.html',{'user':request.user})
+    else:
+        return redirect('login')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+    
+
+def pass_change(request):
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            form=PasswordChangeForm(user=request.user,data=request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request,form.user) #user er pass kora data automatic change hoe jabe.
+                return redirect('profile')
+        
+        else:
+            form=PasswordChangeForm(user=request.user)
+        return render(request,'./passchange.html',{'form':form})
+    else:
+        return redirect('login')
+            
+
+def pass_change_2(request):
+    if request.user.is_authenticated:        
+        if request.method=='POST':
+            form=SetPasswordForm(user=request.user,data=request.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request,form.user)
+                return redirect('profile')
+        else:
+            form=SetPasswordForm(user=request.user)
+        return render(request,'./passchange.html',{'form':form})
+    else:
+        return redirect('login')
