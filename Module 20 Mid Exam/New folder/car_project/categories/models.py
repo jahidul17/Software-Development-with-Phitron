e90881511f712a0f05875738f6155d5a81d27972
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2024-12-18 02:07:19 +0600

    Easy Programming

--- Module 20 Mid Exam/New folder/car_project/categories/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Brand(models.Model):
-    name=models.CharField(max_length=200)
-    slug=models.SlugField(max_length=200,unique=True,null=True,blank=True)
-    
-    def __str__(self) -> str:
-        return self.name
--- Module 20 Mid Exam/New folder/car_project/categories/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from .import views
-
-
-urlpatterns = [
-    path('brand/',views.add_brand, name='add_brand')
-]
